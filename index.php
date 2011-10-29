396c46c7c67de1a761b21bc5b626e4845abd24f1
Julian Qian <junist@gmail.com>
2011-10-30 01:08:56 +0800

    fixed index.php, only files ended with suffix .pdf and .mobi are accepted by amazon proxies.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// index.php --- Time-stamp: <Julian Qian 2011-10-29 20:18:27>
+// index.php --- Time-stamp: <Julian Qian 2011-10-30 01:07:50>
 // Copyright 2011 Julian Qian
 // Author: junist@gmail.com
 // Version: $Id: index.php,v 0.0 2011/08/28 16:12:40 jqian Exp $
@@ -14,11 +14,13 @@ if(empty($_GET["id"])){
     /* fetch documents recent 1 month */
     $sql = "select * from files where downloaded=0 and timestamp>strftime('%s', 'now')-2592000 limit 30;";
     foreach($db->query($sql) as $entry){
-        echo sprintf("http://%s%s?id=%s\n", $_SERVER["HTTP_HOST"],
-                $_SERVER["REQUEST_URI"], $entry["fileid"]);
+        /* the url MUST be ended with the suffix of pdf or mobi. */
+        echo sprintf("http://%s%s?id=%s%s\n", $_SERVER["HTTP_HOST"],
+                $_SERVER["REQUEST_URI"], $entry["fileid"], $entry["filetype"]);
     }
 }else{
-    $fileid = $_GET["id"];
+    $query = explode(".", $_GET["id"]);
+    $fileid = $query[0];
     $sql = "select * from files where fileid='$fileid' limit 1;";
     foreach($db->query($sql) as $entry){
         if($entry["filetype"] == ".pdf"){
@@ -29,14 +31,14 @@ if(empty($_GET["id"])){
             header("HTTP/1.0 404 Not Found");
             die();
         }
-        header("Content-Disposition: attachment; filename=\"".urlencode($entry["filename"]).$entry["filetype"]."\"");
+        header("Content-Disposition: attachment; filename=".urlencode($entry["filename"]).$entry["filetype"]);
         header("Content-Length: ".$entry["filesize"]);
         $filepath = whisper_path($entry["fileid"].$entry["filetype"]);
         readfile($filepath);
-		/* mark the downloaded file */
-		$sql = "update files set downloaded=1 where fileid='".$entry["fileid"]."';";
-		$db->query($sql);
-		break;
+        /* mark the downloaded file */
+        $sql = "update files set downloaded=1 where fileid='".$entry["fileid"]."';";
+        $db->query($sql);
+        break;
     }
 }
 
